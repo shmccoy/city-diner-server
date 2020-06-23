3171009816715a349aa9ba266d530ba27681b207
Sean M <53903852+shmccoy@users.noreply.github.com>
2020-06-23 10:06:14 -0400

    fixed username error

--- src/user-router.js
+++ src/user-router.js
@@ -1,55 +1,24 @@
-const express = require('express')
-const path = require('path')
-const UserService = require('./user-service')
+const express = require("express");
+const path = require("path");
+const UserService = require("./user-service");
 
-const userRouter = express.Router()
-const jsonBodyParser = express.json()
+const userRouter = express.Router();
+const jsonBodyParser = express.json();
 
-userRouter
-  .post('/', jsonBodyParser, (req, res, next) => {
-    const { password, user_name } = req.body
+userRouter.post("/", jsonBodyParser, (req, res, next) => {
+  const { password, user_name } = req.body;
 
-    for (const field of ['user_name', 'password'])
-      if (!req.body[field])
-        return res.status(400).json({
-          error: `Missing '${field}' in request body`
-        })
+  for (const field of ["user_name", "password"])
+    if (!req.body[field])
+      return res.status(400).json({
+        error: `Missing '${field}' in request body`,
+      });
 
-    // TODO: check user_name doesn't start with spaces
+  // TODO: check user_name doesn't start with spaces
 
-    const passwordError = UserService.validatePassword(password)
+  const passwordError = UserService.validatePassword(password);
 
-    if (passwordError)
-      return res.status(400).json({ error: passwordError })
+  if (passwordError) return res.status(400).json({ error: passwordError });
+});
 
-    UserService.hasUserWithUserName(
-      req.app.get('db'),
-      user_name
-    )
-      .then(hasUserWithUserName => {
-        if (hasUserWithUserName)
-          return res.status(400).json({ error: `Username already taken` })
-
-        return UserService.hashPassword(password)
-          .then(hashedPassword => {
-            const newUser = {
-              user_name,
-              password: hashedPassword,
-            }
-
-            return UserService.insertUser(
-              req.app.get('db'),
-              newUser
-            )
-              .then(user => {
-                res
-                  .status(201)
-                  .location(path.posix.join(req.originalUrl, `/${user.id}`))
-                  .json(UserService.serializeUser(user))
-              })
-          })
-      })
-      .catch(next)
-  })
-
-module.exports = userRouter
+module.exports = userRouter;
